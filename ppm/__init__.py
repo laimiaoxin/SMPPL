354f10456e60cc1d9c7883ad0f3f7ce0e596bf0c
laimiaoxin <138202113+laimiaoxin@users.noreply.github.com>
2026-05-27 16:33:13 +0800

    Delete ppm directory

--- ppm/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ftft import PrototypePromptEncoder, GlobalPrototypes, CSM, PromptGenerator
-from .decoder import HierMaskDecoder
-from .transformer import TwoWayTransformer
-from .module import ConvLayer2d, UpSample2d
